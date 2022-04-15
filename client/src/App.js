dbe3a044b766a882c71e14ca439682f78c06bab4
adrianacapua <adrianajcapua@gmail.com>
2022-04-15 18:36:38 +0800

    feat: connect Register component to redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavBar } from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
-import { Register } from './components/auth/Register';
+import Register from './components/auth/Register';
 import { Login } from './components/auth/Login';
 // Redux
 import { Provider } from 'react-redux';
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,9 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { setAlert } from '../../actions/alert';
 
-export const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -20,7 +21,7 @@ export const Register = () => {
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      console.log('Passwords do not match');
+      props.setAlert('Passwords do not match', 'danger');
     } else {
       console.log('SUCCESS');
     }
@@ -69,3 +70,5 @@ export const Register = () => {
     </Fragment>
   )
 }
+
+export default connect(null, { setAlert })(Register);
