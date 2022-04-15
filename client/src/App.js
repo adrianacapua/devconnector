1b7f116acdfd25e01ead2b26fbd2cb0005ea7f23
adrianacapua <adrianajcapua@gmail.com>
2022-04-15 12:55:34 +0800

    feat: register and login router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavBar } from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <NavBar />
-    <Landing />
-  </Fragment>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <section className="container">
+              
+        </section>
+        <Routes>
+          <Route exact path='/' element={ <Landing /> } />
+          <Route exact path="/register" element={ <Register /> } />
+          <Route exact path="/login" element={ <Login /> } />
+        </Routes>
+      </Fragment>
+    </Router>
 );
 
 export default App;
