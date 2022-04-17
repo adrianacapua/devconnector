cbe4ff33758bc64548ac137723c83e067b5a4f49
adrianacapua <adrianajcapua@gmail.com>
2022-04-17 20:56:00 +0800

    feat: load token

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { NavBar } from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import { Login } from './components/auth/Login';
 import Alert from './components/layouts/Alert';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
 import './App.css';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <NavBar />
-        <section className="container">
-            <Alert />
-        </section>
-        <Routes>
-          <Route exact path='/' element={ <Landing /> } />
-          <Route exact path="/register" element={ <Register /> } />
-          <Route exact path="/login" element={ <Login /> } />
-        </Routes>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <section className="container">
+              <Alert />
+          </section>
+          <Routes>
+            <Route exact path='/' element={ <Landing /> } />
+            <Route exact path="/register" element={ <Register /> } />
+            <Route exact path="/login" element={ <Login /> } />
+          </Routes>
+        </Fragment>
+      </Router>
+    </Provider>
+)};
 
 export default App;
