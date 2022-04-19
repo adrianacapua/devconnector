adf19c3b52fa1582bbf114dc83e6e619ff4c8c8e
adrianacapua <adrianajcapua@gmail.com>
2022-04-19 18:07:43 +0800

    feat: PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
 import NavBar from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
 
@@ -28,16 +30,21 @@ const App = () => {
       <Router>
         <Fragment>
           <NavBar />
+          <Routes>
+            <Route exact path='/' element={ <Landing /> } />
+          </Routes>
           <section className="container">
             <Routes>
               <Route exact path="/register" element={ <Register /> } />
               <Route exact path="/login" element={ <Login /> } />
+              <Route exact path="/dashboard" element={ 
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              } />
             </Routes>
-              <Alert />
+            <Alert />
           </section>
-          <Routes>
-            <Route exact path='/' element={ <Landing /> } />
-          </Routes>
         </Fragment>
       </Router>
     </Provider>
