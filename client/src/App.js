3e2660345f65dc82739e0509afa6d6715b91671f
adrianacapua <adrianajcapua@gmail.com>
2022-04-19 17:05:42 +0800

    feat: implement auth links and guest links

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { NavBar } from './components/layouts/NavBar';
+import NavBar from './components/layouts/NavBar';
 import { Landing } from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
