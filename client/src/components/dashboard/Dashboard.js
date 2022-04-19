0ec56313de1c3fe7cab1c1692a6949c26ab448e6
adrianacapua <adrianajcapua@gmail.com>
2022-04-19 18:35:59 +0800

    feat: get profile via dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = props => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect (() => {
+    getCurrentProfile();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>Dashboard</div>
   )
 }
 
-Dashboard.propTypes = {}
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
 
-export default Dashboard
+const mapStateProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
+
+export default connect(mapStateProps, { getCurrentProfile })(Dashboard);
